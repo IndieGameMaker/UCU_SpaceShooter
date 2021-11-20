3a3b127e5c75dc97bfa9b1d4dffe2d116f83f3f4
JaeHyun Lee <myleje@gmail.com>
2021-11-20 15:21:05 +0900

    .

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
+
     // 충돌했을 때 1번 호출되는 콜백메소드
     void OnCollisionEnter(Collision coll)
     {
@@ -12,6 +14,15 @@ public class RemoveBullet : MonoBehaviour
         {
             // 총알을 삭제
             Destroy(coll.gameObject);
+
+            // 충돌 좌표
+            Vector3 pos = coll.GetContact(0).point;
+            // 충돌 지점의 법선 벡터
+            Vector3 _normal = coll.GetContact(0).normal;
+            // 법선 벡터가 이루는 각도를 쿼터니언 타입으로 변환
+            Quaternion rot = Quaternion.LookRotation(-1 * _normal);
+            // 스파크 이펙트 생성
+            Instantiate(sparkEffect, pos, rot);
         }
     }
 }

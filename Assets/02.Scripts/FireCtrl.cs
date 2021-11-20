0f0edbb11ea2b8bb912a5a5296a382984f4c56f8
JaeHyun Lee <myleje@gmail.com>
2021-11-20 15:56:59 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class FireCtrl : MonoBehaviour
 {
-    public Transform firePos;
     public GameObject bulletPrefab;
+    public Transform firePos;
+    public AudioClip fireSfx;
+
+    private AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
 
     // 매 프레임 마다 호출되는 메소드(함수)
     void Update()
     {
         if (Input.GetMouseButtonDown(0) == true)
         {
-            // 총알 생성
-            // Instantiate(생성할객체, 위치, 회전각도)
-            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+            Fire();
         }
+    }
+
+    void Fire()
+    {
+        // 총알 생성
+        // Instantiate(생성할객체, 위치, 회전각도)
+        Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        // 총소리 발생
+        audio.PlayOneShot(fireSfx, 0.8f);
     }
 }

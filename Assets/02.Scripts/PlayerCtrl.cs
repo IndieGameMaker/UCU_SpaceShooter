32de3d7e9b1282415787aa62799adaf2b5b6d7c4
JaeHyun Lee <myleje@gmail.com>
2021-11-20 09:50:22 +0900

    지난주 최종 소스

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    public float moveSpeed = 10.0f;
 
     // 1회 호출되는 함수
     void Start()
@@ -16,8 +17,13 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal"); // 좌/우 화살표 키 값을 입력 -1.0f ~ 0.0f ~ +1.0f
         float v = Input.GetAxis("Vertical"); 
-        Debug.Log("h=" + h); //콘솔 뷰에 출력
-        Debug.Log("v=" + v);
+        float r = Input.GetAxis("Mouse X");
+        // Debug.Log("h=" + h); //콘솔 뷰에 출력
+        // Debug.Log("v=" + v);
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        transform.Translate(moveDir.normalized * Time.deltaTime * moveSpeed);
+        transform.Rotate(Vector3.up * Time.deltaTime * r * 80.0f);
     }
 
 }

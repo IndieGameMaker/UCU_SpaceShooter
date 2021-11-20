56601e1156ccad5b81c0ff1454a410e5bc8f0651
JaeHyun Lee <myleje@gmail.com>
2021-11-20 10:25:59 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -36,6 +36,22 @@ public class PlayerCtrl : MonoBehaviour
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         transform.Translate(moveDir.normalized * Time.deltaTime * moveSpeed);
         transform.Rotate(Vector3.up * Time.deltaTime * r * 80.0f);
+
+        PlayerAnimation(h, v);
+    }
+
+    // Player Animation 처리 로직
+    void PlayerAnimation(float h, float v)
+    {
+        // 전진/후진
+        if (v >= 0.1f)
+        {
+            anim.CrossFade("RunF", 0.3f); // 전진 애니메이션
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("RunB", 0.3f); // 후진 애니메이션
+        }
     }
 
 }

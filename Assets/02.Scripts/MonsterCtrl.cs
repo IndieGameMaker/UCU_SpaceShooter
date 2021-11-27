5638cb92536ab4b007c7f58e64015c82f9ce7350
JaeHyun Lee <myleje@gmail.com>
2021-11-27 14:50:59 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -15,7 +15,7 @@ public class MonsterCtrl : MonoBehaviour
     // 추적 사정거리
     [Range(10.0f, 50.0f)]
     public float traceDist = 10.0f;
-    
+
     // 공격 사정거리
     public float attackDist = 2.0f;
 
@@ -24,15 +24,35 @@ public class MonsterCtrl : MonoBehaviour
     [HideInInspector]  //UnityEngine
     public Transform playerTr;
 
+    public bool isDie = false;
+
     void Start()
     {
         monsterTr = GetComponent<Transform>();
         playerTr = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CheckState()
     {
-        
+        while (isDie == false)
+        {
+            // 두 3차원 좌표간의 거리를 측정
+            float distance = Vector3.Distance(monsterTr.position, playerTr.position);
+
+            if (distance <= attackDist)
+            {
+                state = State.ATTACK;
+            }
+            else if (distance <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.IDLE;
+            }
+
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 }

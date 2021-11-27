d70502d4406b0b511808a1aaa77828ddb81398f6
JaeHyun Lee <myleje@gmail.com>
2021-11-27 14:41:45 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -11,6 +11,13 @@ public class MonsterCtrl : MonoBehaviour
     }
     // 몬스터의 상태
     public State state = State.IDLE;
+    
+    // 추적 사정거리
+    [Range(10.0f, 50.0f)]
+    public float traceDist = 10.0f;
+    
+    // 공격 사정거리
+    public float attackDist = 2.0f;
 
     [System.NonSerialized] //C#
     public Transform monsterTr;

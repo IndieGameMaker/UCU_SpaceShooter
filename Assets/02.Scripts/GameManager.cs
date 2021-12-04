7e650a13730486001219cdeff578cb222c444d31
JaeHyun Lee <myleje@gmail.com>
2021-12-04 13:36:07 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -6,23 +6,21 @@ public class GameManager : MonoBehaviour
 {
     public Transform[] points;
     public GameObject monsterPrefab;
+    public float createTime = 3.0f;
 
     void Start()
     {
         points = GameObject.Find("SpawnPointGroup")?.GetComponentsInChildren<Transform>();
-        // monsterPrefab = Resources.Load("Monster") as GameObject;
-        monsterPrefab = Resources.Load<GameObject>("Prefabs/Monster");
-
-        // GameObject obj = GameObject.Find("SpawnPointGroup");  
-        // if (obj != null)
-        // {
-        //     points = obj.GetComponentsInChildren<Transform>();
-        // }     
+        monsterPrefab = Resources.Load<GameObject>("Prefabs/Monster"); 
+        // 반복 호출
+        InvokeRepeating("CreateMonster", 2.0f, createTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateMonster()
     {
-        
+        // 불규칙한 위치를 선정(난수 발생)
+        int idx = Random.Range(1, points.Length); // 25개 0..24
+        // 몬스터 생성 Instantiate
+        Instantiate(monsterPrefab, points[idx].position, Quaternion.identity);
     }
 }
